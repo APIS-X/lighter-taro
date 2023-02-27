0140703036c1f080c6629bfe3a603c5641a74fde
weiwei.guo <weiwei.guo@tongdun.net>
2023-02-27 13:59:16 +0800

    初始化项目模板

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import { Button, Cell } from '@nutui/nutui-react-taro';
+import './index.scss';
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return (
+      <div className='nutui-react-demo'>
+        <div className='index'>欢迎使用 NutUI React 开发 Taro 多端项目。</div>
+        <div className='index'>
+          <Button type='primary' className='btn'>
+            NutUI React Button
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Index;
